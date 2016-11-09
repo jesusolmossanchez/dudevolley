a8f5179dae68962431d042fb3b8742243c8c9e03
Jesús Olmos <jesusolmossanchez@gmail.com>
2016-11-09 20:44:40 +0100

    empezando con juego con twitter

--- juega_twitter.php
+++ juega_twitter.php
@@ -2,18 +2,30 @@
 
 
 session_start(); 
-require "../vendor/autoload.php";
+require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 define("CONSUMER_KEY", "Imviwz5oC86qaucxGMZKusx9T");
 define("CONSUMER_SECRET", "8ZphEwd64bCie4mf70nSlRxEBMsJ2LUJEnpKdbHoCJBB363VwW");
 
-$request_token['oauth_token'] = $_SESSION['oauth_token'];
-$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+ $request_token = [];
+  $request_token['oauth_token'] = $_SESSION['oauth_token'];
+  $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+  if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+      // Abort! Something is wrong.
+  }
 
-$user = $connection->get("account/verify_credentials");
+
+  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+
+  $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
+
+  $_SESSION['access_token'] = $access_token;
+
+  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+  $user = $connection->get("account/verify_credentials");
 
 var_dump($user);
 exit;
