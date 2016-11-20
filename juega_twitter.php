d4b72a54cf376fcbbeda79354ad22065f5222a42
Jesús Olmos <jesusolmossanchez@gmail.com>
2016-11-20 16:46:41 +0100

    twitter con cookie

--- juega_twitter.php
+++ juega_twitter.php
@@ -108,7 +108,9 @@ class CircleCrop{
 
 	$access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
 
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_REQUEST['oauth_token'], $access_token['oauth_token_secret']);
+	$_SESSION['access_token'] = $access_token;
+
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
 	$user = $connection->get("account/verify_credentials");
 
