a8b8cd48ab293ffaf5e184145c9e1d20ad2ac5dd
Jesús Olmos <jesusolmossanchez@gmail.com>
2016-11-20 16:40:59 +0100

    twitter en cookie

--- twitter/index.php
+++ twitter/index.php
@@ -15,7 +15,7 @@ $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
 $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
 
 setcookie("oauth_token", $request_token['oauth_token'], time()+3600,'/');
-setcookie("oauth_token_secret", $request_token['oauth_token'], time()+3600,'/');
+setcookie("oauth_token_secret", $request_token['oauth_token_secret'], time()+3600,'/');
 
 $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
 
