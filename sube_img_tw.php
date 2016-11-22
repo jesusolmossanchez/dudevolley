922fd049307aeb10ebc3a9615cee4c08bbee99b0
jesusolmossanchez <jesusolmossanchez@gmail.com>
2016-11-22 19:28:06 +0100

    status twitter con mencion

--- sube_img_tw.php
+++ sube_img_tw.php
@@ -4,20 +4,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
-
 if (isset($_POST['imagen'])){
 	$imagen64 = $_POST['imagen'];
-
 	$id = uniqid();
-
 	$filename_path = $id.".png"; 
-
 	$img = str_replace('data:image/png;base64,', '', $imagen64);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
-
 	file_put_contents("img_share/".$filename_path, $data);
-
 	echo "img_share/".$filename_path;
 	
 	
@@ -27,14 +21,14 @@ if (isset($_POST['imagen'])){
 	define("CONSUMER_SECRET", "8ZphEwd64bCie4mf70nSlRxEBMsJ2LUJEnpKdbHoCJBB363VwW");
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
 
+
+	$usuario = $_POST["usuario"];
+	$puntos1 = $_POST["puntos1"];
+	$puntos2 = $_POST["puntos2"];
 	
 	$post_data = array('media_data' => $data);
-
 	$media = $connection->post("media/upload", $post_data);
-
-	$post_data2 = array('status' => 'Le estoy dando palpelo a alguien', 'media_id' => $media->media_id, );
-
+	$post_data2 = array('status' => 'Le he dado palpelo a '.$usuario, 'media_id' => $media->media_id, );
 	$media = $connection->post("statuses/update", $post_data2);
 	
-
 }
