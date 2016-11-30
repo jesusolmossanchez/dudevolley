96de03e182a42e68957927f812cd0aab17fe5bf6
Jesús Olmos <jesusolmossanchez@gmail.com>
2016-11-30 23:10:15 +0100

    fix reta a un colega

--- js/GameMultiplayer.js
+++ js/GameMultiplayer.js
@@ -51,6 +51,8 @@ DudeVolley.GameMultiplayer.prototype = {
             $("#socket_nombre").focus();
 
 
+            var id_socket = this.id;
+
 
             //$("#soy_el_uno").show();
             eljuego.input.keyboard.removeKey(Phaser.Keyboard.D);
@@ -68,7 +70,7 @@ DudeVolley.GameMultiplayer.prototype = {
                 SUPERPIKA2 = eljuego.input.keyboard.addKey(Phaser.Keyboard.Z);
 
                 if(window.te_reto){
-                    Player1 = new Player(eljuego,'cpu', null, this.id);
+                    Player1 = new Player(eljuego,'cpu', null, id_socket);
                     Player1.nombre = $("#socket_nombre").val();
                     socket.emit("player_ready_privada", {nombre: Player1.nombre, privada: window.te_reto});
                 }
