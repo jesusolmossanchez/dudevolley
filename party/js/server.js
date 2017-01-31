bf8d3f3eaed13bdc191b3d0bd92e6bbeec756fbf
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-01-31 17:09:19 +0100

    Cambios en el server de party

--- party/js/server.js
+++ party/js/server.js
@@ -2,13 +2,9 @@ var io = require('socket.io')(8081);
 var util = require("util");
 
 
-Player = require("./Player").Player;
-//var util = require("util");
 var setEventHandlers = function() {
     io.sockets.on("connection", onSocketConnection);
 };
-
-
 
 
 function onSocketConnection(client) {
@@ -18,12 +14,12 @@ function onSocketConnection(client) {
     client.on("accion_pulsado", onAccionPulsado);
     client.on("accion_suelto", onAccionSuelto);
     client.on("teclas", onTeclas);
-};
+}
 
 //Desconexión
 function onClientDisconnect() {
-    
-};
+    //TODO -- quitar del array de conexiones (tiene parametros? como saco la room?)
+}
 
 //Prepara partida
 function onPreparaParty(room) {
@@ -31,38 +27,44 @@ function onPreparaParty(room) {
     var mi_room = room;
 	this.join(mi_room);
 	this.room = mi_room;
-};
+}
 
 //Entra un mando
 function onNewController(data) {
-	console.log("nuevo controlador: ",data)
+	console.log("nuevo controlador: ",data);
     var mi_room = data.id_room;
-    players[mi_room] = [];
-    players[mi_room].push(data.nombre);
     this.join(mi_room);
     this.room = mi_room;
-    io.to(mi_room).emit("new_player", this.id);
-};
+
+    jugador = {};
+    jugador.id = this.id;
+    jugador.nombre = data.nombre;
+
+    players[mi_room].push(jugador);
+
+
+    //TODO - Ver si ya hay dos y hacer otra cosa...
+    io.to(mi_room).emit("new_player", jugador);
+}
 
 //propaga la pulsacion
 function onAccionPulsado(data) {
-	console.log("pulso: ",data);
     mi_room = data.room;
     io.to(mi_room).emit("accion_pulsado", data.id);
-};
+}
 
 //propaga la pulsacion
 function onAccionSuelto(data) {
     mi_room = data.room;
     io.to(mi_room).emit("accion_suelto", data.id);
-};
+}
 
 
 //propaga el movimiento
 function onTeclas(data) {
     mi_room = data.room;
     io.to(mi_room).emit("recibeteclas", data);
-};
+}
 
 
 
@@ -70,6 +72,6 @@ function init() {
     players = [];
     io.set("transports", ["websocket"]);
     setEventHandlers();
-};
+}
 
 init();
