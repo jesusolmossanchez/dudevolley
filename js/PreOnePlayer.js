7c7d180c86294023594b0b8105f35a660fd51869
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-07-07 15:23:11 +0200

    luismi

--- js/PreOnePlayer.js
+++ js/PreOnePlayer.js
@@ -6,8 +6,13 @@ DudeVolley.PreOnePlayer.prototype = {
 
 	preload: function (){
 		if (this.game.normalplayer){
-			this.load.spritesheet('player1','assets/default_player.png',80,110);
-			this.load.spritesheet('player1_chick','assets/default_player_chick.png',80,110);
+			if(window.luismi){
+				this.load.spritesheet('player1','assets/luismi.png',80,110);
+			}
+			else{
+				this.load.spritesheet('player1','assets/default_player.png',80,110);
+				this.load.spritesheet('player1_chick','assets/default_player_chick.png',80,110);
+			}
 		}else{
 			this.load.spritesheet('player1', this.game.ruta_jugador, 80, 110);
 		}
